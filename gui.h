27908184201c6a748a4b5ac6b1d428833947f722
raggyftw <hansenoliie@gmail.com>
2017-05-29 20:49:42 +0200

    Update gui.h
    added comments

--- gui.h
+++ gui.h
@@ -7,31 +7,31 @@
 
 
 namespace Ui {
-class GUI;
-}
+class GUI;//Declaring that the namespace of the Ui is within GUI
+}//End of namespace Ui
 
-class GUI : public QDialog
+class GUI : public QDialog //The GUI class inherits all public atributes from the QDialog Class
 {
     Q_OBJECT
 
 public:
-    explicit GUI(QWidget *parent = 0);
-    ~GUI();
-     Ui::GUI *ui;
+    explicit GUI(QWidget *parent = 0);// Declaring contructor
+    ~GUI(); // Declaring destructor
+     Ui::GUI *ui; //Setting up the UI
 
 
 private slots:
-     void on_button1_clicked();
-     void on_button2_clicked();
-     void on_button3_clicked();
-     void on_button4_clicked();
+     void on_button1_clicked();//Creating method for clicking on button 1
+     void on_button2_clicked();//Creating method for clicking on button 2
+     void on_button3_clicked();//Creating method for clicking on button 3
+     void on_button4_clicked();//Creating method for clicking on button 4
 
 private:
-     handler hdl;
-     std::vector<double> teams;
-     void update_score(int);
+     handler hdl;//Creating a object of the handler class
+     std::vector<double> teams;//Creating vector to store the teams point's
+     void update_score(int); //method to update the score
 
 
-};
+};//End of the GUI class
 
 #endif // GUI_H
