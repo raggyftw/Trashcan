6b3078961e40ae34b6fb959c28059ca560796c37
raggyftw <hansenoliie@gmail.com>
2017-05-29 21:15:16 +0200

    Update main.cpp
    added comments

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    GUI w;
-    w.show();
+    GUI w; //Creates an object of the GUI class called w
+    w.show(); //calls the show method from the GUI class
 
     return a.exec();
-}
+}//End of main
