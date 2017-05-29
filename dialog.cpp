69e659acc8014557e20ec1e81c912c6ef46d5032
raggyftw <hansenoliie@gmail.com>
2017-05-29 20:29:42 +0200

    Update dialog.cpp
    added comments

--- dialog.cpp
+++ dialog.cpp
@@ -5,10 +5,10 @@ Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Dialog)
 {
-    ui->setupUi(this);
-}
+    ui->setupUi(this); //Declaring that the UI will be established in this file
+}// End of constructor
 
 Dialog::~Dialog()
 {
     delete ui;
-}
+}// End of descructor 
