1e0172f0da9d7c2cc94182d9ee1a11260821a7e7
raggyftw <hansenoliie@gmail.com>
2017-05-29 20:26:15 +0200

    Update arduino_data.cpp
    Added comments

--- arduino_data.cpp
+++ arduino_data.cpp
@@ -5,7 +5,7 @@
 #include <QDebug>
 #include <QMessageBox>
 
-arduino_data::arduino_data(QWidget *parent) :
+arduino_data::arduino_data(QWidget *parent) : // Constructor used to establish serial connection.
     QMainWindow(parent)
 {
 
@@ -51,29 +51,26 @@ arduino_data::~arduino_data()
 void arduino_data::readSerial()
 {
 
-    QStringList buffer_split = serialBuffer.split(",");
-    //qDebug() << buffer_split;
+    QStringList buffer_split = serialBuffer.split(","); //Splits the QSTring into several strings, sepperated by ","
 
-
-    if(buffer_split.length() < 3){
-        serialData = arduino->readAll();
-        serialBuffer = serialBuffer + QString::fromStdString(serialData.toStdString());
-        serialData.clear();
-    }else{
-        serialBuffer = "";
-        parsed_weight = buffer_split[1];
-        weight_value =  (parsed_weight.toDouble());
-        parsed_height = buffer_split[0];
-        height_value =  (parsed_height.toDouble());
-        qDebug()<<height_value<<weight_value;
-    }
-
-}
+    if(buffer_split.length() < 3){ //Checks if the length of the QString is less than 3
+        serialData = arduino->readAll();//Reads all available data
+        serialBuffer = serialBuffer + QString::fromStdString(serialData.toStdString());//Sends the data to the buffer
+        serialData.clear();//Clears the datastream
+    }//End of if-statement
+    else{
+        serialBuffer = ""; //Sets the buffer to be empty
+        parsed_weight = buffer_split[1]; //Stores the 2nd element of the Stringlist 
+        weight_value =  (parsed_weight.toDouble()); //converts the stored element to a double
+        parsed_height = buffer_split[0]; //Stores the 1st element of the Stringlist
+        height_value =  (parsed_height.toDouble());//converts the stored element to a double
+    }//End of else-statement
+}//End of readserial
 double arduino_data::get_weight(){
-weight = weight_value*-1;
-return weight;
+weight = weight_value*-1; //stores the weight value and changes the sign of the stored data
+return weight; //Returns the weight value
 }
 double arduino_data::get_height(){
-height= height_value;
-return height;
+height= height_value; //Stores the height value.
+return height; //returns the stored value
 }
